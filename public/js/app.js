c30c89072dc7c37d97e57531fafa9081dd7e70a9
Michael Karr <mdkarr01@gmail.com>
2019-09-20 15:30:23 -0500

    edit styling and adding to about

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,23 @@
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 
-const errorMessage = document.querySelector('#error');
-const forecastMessage = document.querySelector('#forecast');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', e => {
   e.preventDefault();
   const location = search.value;
 
-  errorMessage.textContent = 'Loading...';
-  errorMessage.textContent = '';
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
 
   fetch(`/weather?address=${location}`).then(res => {
     res.json().then(data => {
       if (data.error) {
-        errorMessage.textContent = data.error;
+        messageOne.textContent = data.error;
       } else {
-        forecastMessage.textContent = `The forecast for ${data.location} will be ${data.forecast}`;
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
         // console.log(data.forecast);
         // console.log(data.location);
       }
